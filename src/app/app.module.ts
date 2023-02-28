f6683c7397741c0768e687c9dbf632de8cb84a65
asytkin <anast_naimushina@mail.ru>
2023-02-28 20:54:10 +0300

    добавила роуты, переименовала css файлы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
+
+const appRoutes: Routes =[
+  { path: '', component: AppComponent},
+  { path: 'productlist', component: ProductListComponent, pathMatch:'full'},
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +25,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgFor
+    NgFor,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
