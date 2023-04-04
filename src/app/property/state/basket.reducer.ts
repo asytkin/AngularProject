a68846135bd520febf511ee54febf97453bc6a2f
asytkin <anast_naimushina@mail.ru>
2023-04-04 13:47:55 +0500

    fix ADD_ITEMS

--- src/app/property/state/basket.reducer.ts
+++ src/app/property/state/basket.reducer.ts
@@ -1,13 +1,13 @@
 import * as basketActions from './basket.actions';
 import { BasketItem } from '../../model/basketitem';
 import * as fromRoot from '../../state/app-state';
-import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { of } from 'rxjs';
 
 export interface BasketState {
-  items: BasketItem[],
-  error: string
-};
+  items: BasketItem[];
+  error: string;
+}
 
 export interface AppState extends fromRoot.AppState {
   basket: BasketState;
@@ -15,70 +15,79 @@ export interface AppState extends fromRoot.AppState {
 
 export const initialState: BasketState = {
   items: [],
-  error: ""
+  error: '',
 };
 
-export function basketReducer(state = initialState, action: basketActions.Action): BasketState {
+export function basketReducer(
+  state = initialState,
+  action: basketActions.Action
+): BasketState {
   switch (action.type) {
-    case basketActions.BasketActionTypes.ADD_ITEMS:
-      {
-        let product = action.payload.item;
-        if (state.items.find((i) => i.item.id == product.id) == null) {
-          let newBasketItem: BasketItem = {
-            item: product,
-            count: 1,
-          };
-          state.items.push(newBasketItem);
-          console.log(newBasketItem);
-        } else {
-          let basketItemSameId = state.items.find(
-            (i) => i.item.id == product.id
-          );
-          if (basketItemSameId != null) basketItemSameId.count++;
-        }
+    case basketActions.BasketActionTypes.ADD_ITEMS: {
+      let product = action.payload.item;
+      if (state.items.find((i) => i.item.id == product.id) == null) {
+        let newBasketItem: BasketItem = {
+          item: product,
+          count: 1,
+        };
+
+        console.log(newBasketItem);
+        const itemsToBasket = [...state.items, newBasketItem];
+        return {
+          ...state,
+          items: itemsToBasket,
+        };
+      } else {
+        let basketItemSameId = state.items.find((i) => i.item.id == product.id);
+        if (basketItemSameId != null) basketItemSameId.count++;
 
         return {
           ...state,
-          items: state.items
+          items: state.items,
         };
       }
+    }
     default: {
       return state;
     }
   }
 }
 
-const getBasketFeatureState = createFeatureSelector<BasketState>(
-  "items"
-);
+const getBasketFeatureState = createFeatureSelector<BasketState>('items');
 
 export const getItemsCount = createSelector(
   getBasketFeatureState,
   (state: BasketState) => {
-    const sum: number =
-      state.items.reduce((sum, currentBasketItem) =>
-        sum + Number(currentBasketItem.item.price), 0);
-    return (sum == null ? 0 : sum);
+    const sum: number = state.items.reduce(
+      (sum, currentBasketItem) => sum + Number(currentBasketItem.item.price),
+      0
+    );
+    return sum == null ? 0 : sum;
   }
 );
 
 export const getTotalSum = createSelector(
   getBasketFeatureState,
   (state: BasketState) => {
-    const sum: number =
-      state.items.reduce((sum, currentBasketItem) =>
-        sum + Number(currentBasketItem.item.price) * currentBasketItem.count, 0);
-    return (sum == null ? 0 : sum);
+    const sum: number = state.items.reduce(
+      (sum, currentBasketItem) =>
+        sum + Number(currentBasketItem.item.price) * currentBasketItem.count,
+      0
+    );
+    return sum == null ? 0 : sum;
   }
 );
 
 export const getItems = createSelector(
   getBasketFeatureState,
-  (state: BasketState) => { return state.items; }
+  (state: BasketState) => {
+    return state.items;
+  }
 );
 
 export const getError = createSelector(
   getBasketFeatureState,
-  (state: BasketState) => { return state.error; }
+  (state: BasketState) => {
+    return state.error;
+  }
 );
-
